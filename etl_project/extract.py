63919131da48edce2d4a3b25128a444a2c4541b2
Abhi-Marshall <marshallabhi.2004@gmail.com>
2026-03-20 09:01:49 +0000

    testing push from databricks to github

--- etl_project/extract.py
+++ etl_project/extract.py
@@ -1,7 +1,9 @@
 import pandas as pd
 
+print("Hello Brother")
 def extract_data():
     # read input data from csv file
     data = pd.read_csv("input.csv")
     print("Data extracted")
     return data
+
